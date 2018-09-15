ab73ebdd1c26826eede38d779c322357f0e558e2
Nasser Sanou <techman@Nassers-MacBook-Pro.local>
2018-09-14 20:16:57 -0400

    test variable && window objects

---
+++ index.js
@@ -0,0 +1,12 @@
+var a = 5,
+    b = 2,
+    c = 3,
+    d = 'Hello World',
+    e = 'welcome to the scripts world';
+
+var word = 'bird';
+
+
+    console.log(a);
+    console.log(e);
+      console.log(window.word);
